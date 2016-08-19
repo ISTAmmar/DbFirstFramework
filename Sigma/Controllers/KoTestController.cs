dd7b4a6d2ea8d6eec9cb286ee18ccade912b9b11
ISTAmmar <akhtar.ammar1@gmail.com>
2016-08-19 18:10:44 +0500

    remove Angular and add knockout

---
+++ Sigma/Controllers/KoTestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Sigma.Models;
+
+namespace Sigma.Controllers
+{
+    public class KoTestController : Controller
+    {
+        // GET: KoTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Save(Person person)
+        {
+            var message = string.Format("Saved {0} {1}", person.FirstName, person.LastName);
+            return Json(new {message});
+        }
+    }
+}
---
+++ Sigma/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Sigma.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<Friend> Friends { get; set; }
+    }
+    public class Friend
+    {
+        public string Name { get; set; }
+        public bool IsOnTwitter { get; set; }
+        public string TwitterName { get; set; }
+    }
+}
